ddd059b7f25b11b46c1c0d8c081875d30cff4191
SakibulHasaan <sakibhasan5271@gmail.com>
2021-02-26 21:13:22 +0600

    Loaded and displayed data from FakeData

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import fakeData from "../../fakeData"
+import './Shop.css'
+
+const Shop = () => {
+    const firstTen = fakeData.slice(0,10);
+    const [products, setProducts]= useState(firstTen);
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(product => <li>{product.name}</li>)
+                    }
+                </ul>
+            </div>
+
+            <div className="cart-container">
+                <h3>
+                    this is Cart
+                </h3>
+            </div>
+            
+
+
+        </div>
+    );
+};
+
+export default Shop;
